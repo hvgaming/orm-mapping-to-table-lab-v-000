9fec3c7db9f3b8a6c775bf9c8704032dd16c2583
hugo valois <hvgaming@yahoo.com>
2018-11-07 21:59:35 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,8 @@
 require_relative '../config/environment.rb'
 class Student
   attr_accessor :id, :name, :grade
-   
+   def initialize(name, grade)
+    @name = name
     @grade = grade
   end
    def self.create(name, grade)
